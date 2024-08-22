a21f009695dafcba5f8cabac33c35e9119d7cc00
ivanJoshua98 <ivan.sanmartin98@gmail.com>
2024-08-22 15:37:15 -0300

    Set up backend

---
+++ backend-apc/src/test/java/ar/edu/unq/apc/ApcApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.apc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
