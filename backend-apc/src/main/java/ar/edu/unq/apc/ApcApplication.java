19935b87a823b98d18884fdfd26c4ac9bdb84c63
ivanJoshua98 <ivan.sanmartin98@gmail.com>
2024-08-30 23:29:54 -0300

    Se configura proyecto para utilizar postgres como base de datos

--- backend-apc/src/main/java/ar/edu/unq/apc/ApcApplication.java
+++ backend-apc/src/main/java/ar/edu/unq/apc/ApcApplication.java
@@ -2,8 +2,9 @@ package ar.edu.unq.apc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ApcApplication {
 
 	public static void main(String[] args) {
