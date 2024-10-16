e8d1f06d60a7c3d742f5125cbedf08975a4061d2
ivanJoshua98 <ivan.sanmartin98@gmail.com>
2024-10-16 00:10:21 -0300

    Se eliminan clases para hacer un refactor

--- backend-apc/src/main/java/ar/edu/unq/apc/model/exceptions/IncorrectPasswordException.java
+++
@@ -1,7 +0,0 @@
-package ar.edu.unq.apc.model.exceptions;
-
-public class IncorrectPasswordException extends RuntimeException {
-    public IncorrectPasswordException(String message) {
-        super(message);
-    }
-}
--- backend-apc/src/main/java/ar/edu/unq/apc/model/exceptions/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package ar.edu.unq.apc.model.exceptions;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
