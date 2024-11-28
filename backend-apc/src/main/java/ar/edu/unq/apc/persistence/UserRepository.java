262f1ea0f8b673daa776fee320396dd0489817e9
ivanJoshua98 <ivan.sanmartin98@gmail.com>
2024-11-28 04:56:20 -0300

    Se crea endpoint para reportes del sistema de usuarios con mas compras

--- backend-apc/src/main/java/ar/edu/unq/apc/persistence/UserRepository.java
+++ backend-apc/src/main/java/ar/edu/unq/apc/persistence/UserRepository.java
@@ -1,12 +1,16 @@
 package ar.edu.unq.apc.persistence;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import ar.edu.unq.apc.model.CartState;
 import ar.edu.unq.apc.model.UserModel;
+import ar.edu.unq.apc.model.UserWithMostPurchases;
 
 @Repository
 public interface UserRepository  extends JpaRepository<UserModel, UUID> {
@@ -18,6 +22,17 @@ public interface UserRepository  extends JpaRepository<UserModel, UUID> {
     Boolean existsByUserName(String userName);
 
     Optional<UserModel> findByUserName(String userName);
+
+    @Query(nativeQuery = true, value = "SELECT id, email, user_name, purchases_count FROM user_model " + 
+            "INNER JOIN (SELECT buyer_id, COUNT(buyer_id) AS purchases_count " + 
+                        "FROM shopping_cart " +
+                        "WHERE cart_state = ?1 " +
+                        "GROUP BY buyer_id HAVING COUNT (buyer_id)>=1 " +
+                        "ORDER BY purchases_count DESC " +
+                        "LIMIT 5) AS top_five " + 
+            "ON user_model.id=top_five.buyer_id " +
+            "ORDER BY purchases_count DESC")
+    List<UserWithMostPurchases> findUsersWithMostPurchases(CartState state);
 
 
 }
---
+++ backend-apc/src/main/java/ar/edu/unq/apc/webService/SystemReportController.java
@@ -0,0 +1,41 @@
+package ar.edu.unq.apc.webService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ar.edu.unq.apc.model.UserWithMostPurchases;
+import ar.edu.unq.apc.service.UserService;
+import ar.edu.unq.apc.webService.dto.UserWithMostPurchasesDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "System reports services", description = "Manage reports of the application")
+@RestController
+@RequestMapping("/apc")
+public class SystemReportController {
+
+    @Autowired
+    private UserService userService;
+
+    
+    @Operation(summary = "Get top five users with most purchases")
+    @GetMapping("/reports/users-with-most-purchases")
+    public ResponseEntity<List<UserWithMostPurchasesDTO>> getTopFiveUsersWithMostPurchases(){
+        List<UserWithMostPurchases> topFive = this.userService.getUserWithMostPurchases();
+
+        return ResponseEntity.ok().body(topFive.stream().map(this::convertUserEntityToUserDTO).toList());
+    }
+
+    private UserWithMostPurchasesDTO convertUserEntityToUserDTO(UserWithMostPurchases user){
+        return new UserWithMostPurchasesDTO(user.getId(), 
+                                            user.getUserName(), 
+                                            user.getEmail(), 
+                                            user.getPurchasesCount());
+    }
+    
+}
