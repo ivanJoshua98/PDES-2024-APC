dca09b3da69f95e600c4472352d790a001b02da8
ivanJoshua98 <ivan.sanmartin98@gmail.com>
2024-12-10 22:05:14 -0300

    Se resuelve problema de cors

--- backend-apc/src/main/java/ar/edu/unq/apc/security/SecurityConfig.java
+++ backend-apc/src/main/java/ar/edu/unq/apc/security/SecurityConfig.java
@@ -50,6 +50,8 @@ public class SecurityConfig {
 	CorsConfigurationSource corsConfigurationSource(){
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:5000"));
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:9090"));
 		configuration.setAllowedMethods(Arrays.asList("*"));
 		configuration.setExposedHeaders(Arrays.asList("*"));
 		configuration.setAllowedHeaders(Arrays.asList("*"));
