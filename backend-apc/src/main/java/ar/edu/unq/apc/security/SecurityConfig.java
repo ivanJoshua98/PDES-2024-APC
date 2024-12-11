feb43373ce254fc16aebe3f1bceec75a1529fd84
ivanJoshua98 <ivan.sanmartin98@gmail.com>
2024-12-10 22:28:40 -0300

    Se corrige error de sintaxis con respecto a los cors

--- backend-apc/src/main/java/ar/edu/unq/apc/security/SecurityConfig.java
+++ backend-apc/src/main/java/ar/edu/unq/apc/security/SecurityConfig.java
@@ -49,9 +49,7 @@ public class SecurityConfig {
     @Bean
 	CorsConfigurationSource corsConfigurationSource(){
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
 		configuration.setAllowedOrigins(Arrays.asList("http://localhost:5000"));
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:9090"));
 		configuration.setAllowedMethods(Arrays.asList("*"));
 		configuration.setExposedHeaders(Arrays.asList("*"));
 		configuration.setAllowedHeaders(Arrays.asList("*"));
